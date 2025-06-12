3fd5ebc345c7d1a240dacd40a37ee00ae15d5405
YeQiu <YeQiu@example.com>
2025-06-11 19:55:40 -0600

    Constructor Injection

--- src/main/java/com/cs/store/OrderService.java
+++ src/main/java/com/cs/store/OrderService.java
@@ -1,8 +1,15 @@
 package com.cs.store;
 
 public class OrderService {
+
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+
     public void placeOrder(){
-        var paymentService = new StripePaymentService();
+       // var paymentService = new StripePaymentService();
         paymentService.processPayment(10);
     }
 }
---
+++ src/main/java/com/cs/store/PayPalPaymentService.java
@@ -0,0 +1,10 @@
+package com.cs.store;
+
+public class PayPalPaymentService implements PaymentService {
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("PayPal");
+        System.out.println("Amount: " + amount);
+    }
+}
