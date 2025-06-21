7ebf0d6b2768ac2fde7cb846f4418b78442ac84d
YeQiu <YeQiu@example.com>
2025-06-20 18:04:21 -0600

    RestService First Test

--- src/main/java/com/cs/store/PayPalPaymentService.java
+++ src/main/java/com/cs/store/PayPalPaymentService.java
@@ -8,6 +8,12 @@ public class PayPalPaymentService implements PaymentService {
     @Override
     public void processPayment(double amount) {
         System.out.println("PayPal");
+
+        int num1 = 7;
+        int num2 = 2;
+
+        double result = num1 / num2;
+        System.out.println(result);
         System.out.println("Amount: " + amount);
     }
 }
